c34d5ba32a89759dc438ff631fdd3726d2a8f56c
yeoularu <yeoularu@gmail.com>
2025-10-10 20:12:50 +0900

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,39 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@i-starred-it/db";
+import * as schema from "@i-starred-it/db/schema/auth";
+import { env } from "cloudflare:workers";
+
+export const auth = betterAuth({
+	database: drizzleAdapter(db, {
+		provider: "sqlite",
+
+		schema: schema,
+	}),
+	trustedOrigins: [env.CORS_ORIGIN],
+	emailAndPassword: {
+		enabled: true,
+	},
+	// uncomment cookieCache setting when ready to deploy to Cloudflare using *.workers.dev domains
+	// session: {
+	//   cookieCache: {
+	//     enabled: true,
+	//     maxAge: 60,
+	//   },
+	// },
+	secret: env.BETTER_AUTH_SECRET,
+	baseURL: env.BETTER_AUTH_URL,
+	advanced: {
+		defaultCookieAttributes: {
+			sameSite: "none",
+			secure: true,
+			httpOnly: true,
+		},
+		// uncomment crossSubDomainCookies setting when ready to deploy and replace <your-workers-subdomain> with your actual workers subdomain
+		// https://developers.cloudflare.com/workers/wrangler/configuration/#workersdev
+		// crossSubDomainCookies: {
+		//   enabled: true,
+		//   domain: "<your-workers-subdomain>",
+		// },
+	},
+});
